9f4cd9ed8521f2f48e7d45b7ca531693583d39c9
takao-h <hayashi_takao@cyberbuzz.co.jp>
2017-11-29 00:51:57 +0900

    Add actor request & response

---
+++ app/chat/actors/RequestActor.scala
@@ -0,0 +1,29 @@
+package chat.actors
+
+import akka.actor._
+import play.api.libs.json._
+import chat._
+
+/**
+  * Created by ruth on 2017/11/29.
+  */
+
+class RequestActor(out: ActorRef) extends Actor {
+
+  override def receive: Receive = {
+    case msg: JsValue => {
+      val user = (msg \ "user").as[String]
+      val text = (msg \ "text").as[String]
+      out ! Speech(user, text)
+    }
+  }
+
+  override def preStart() = println(s"Start ${out.path}")
+
+  override def postStop() = println(s"Stop ${out.path}")
+
+}
+
+object RequestActor {
+  def props(out: ActorRef): Props = Props(new RequestActor(out))
+}
